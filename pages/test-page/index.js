9c0ca9ac3d62fe0da71d2b36f187a47572b60a74
Ferris Armstrong <fda@bu.edu>
2022-02-28 23:58:43 -0500

    init

---
+++ pages/test-page/index.js
@@ -0,0 +1,7 @@
+import styles from "../../styles/Home.module.css";
+
+export default function Page() {
+    return (
+        <h1 className={styles.title}>This is a route within the home app</h1>
+    );
+}
