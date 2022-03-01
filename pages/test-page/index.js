9343d1328d90e63e22b7b973fb6a3ae204c6f2db
Ferris Armstrong <fda@bu.edu>
2022-03-01 00:24:07 -0500

    add cookie

--- pages/test-page/index.js
+++ pages/test-page/index.js
@@ -1,7 +1,30 @@
+import React from "react";
 import styles from "../../styles/Home.module.css";
+import Cookies from "js-cookie";
 
 export default function Page() {
+    const [text, setText] = React.useState("");
+
     return (
-        <h1 className={styles.title}>This is a route within the home app</h1>
+        <div
+            style={{
+                gap: 64,
+                display: "grid",
+                justifyContent: "center",
+            }}
+        >
+            <h1 className={styles.title}>
+                This is a route within the home app
+            </h1>
+            <div>
+                <input
+                    placeholder="Type Cookie Data"
+                    onChange={(e) => setText(e.currentTarget.value)}
+                />
+                <button onClick={() => Cookies.set("_some_cookie", text)}>
+                    Set Cookie
+                </button>
+            </div>
+        </div>
     );
 }
