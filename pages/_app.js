e80e10e03b2d293b8fceac2dfd038ba76d619d02
Ferris Armstrong <fda@bu.edu>
2022-03-01 12:24:40 -0500

    add ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -16,5 +17,17 @@ function MyApp({ Component, pageProps }) {
         </>
     );
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+MyApp.getInitialProps = async (appContext) => {
+    // calls page's `getInitialProps` and fills `appProps.pageProps`
+    const appProps = await App.getInitialProps(appContext);
+
+    return { ...appProps };
+};
 
 export default MyApp;
